5c19f851387174921dee4df3481ffb159f1c8405
Patidar <sarthakpatidar@gmail.com>
2019-10-19 22:09:53 +0530

    Renaming classes to match Java conventions

--- leetCode/maxProductSubArray.java
+++ leetCode/maxProductSubArray.java
@@ -2,7 +2,7 @@ package leetCode;
 
 import java.util.*;
 
-public class maxProductSubArray {
+public class MaxProductSubArray {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
