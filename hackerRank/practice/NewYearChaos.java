ae81c3faa4df3e6ea4fad89517d6dfb1d28dc73f
Patidar <sarthakpatidar@gmail.com>
2019-11-05 23:37:34 +0530

    Added Hackerrank pratice problem

---
+++ hackerRank/practice/NewYearChaos.java
@@ -0,0 +1,47 @@
+package hackerRank.practice;
+
+import java.util.*;
+
+public class NewYearChaos {
+
+	// Complete the minimumBribes function below.
+	static void minimumBribes(int[] q) {
+		int sum = 0;
+		for (int i = q.length - 1; i >= 0; i--) {
+			if ((q[i] - i - 1) > 2) {
+				System.out.println("Too chaotic");
+				return;
+			}
+			for (int j = Math.max(0, q[i] - 2); j < i; j++)
+				if (q[j] > q[i])
+					sum++;
+		}
+		System.out.println(sum);
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int t = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int tItr = 0; tItr < t; tItr++) {
+			int n = scanner.nextInt();
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			int[] q = new int[n];
+
+			String[] qItems = scanner.nextLine().split(" ");
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			for (int i = 0; i < n; i++) {
+				int qItem = Integer.parseInt(qItems[i]);
+				q[i] = qItem;
+			}
+
+			minimumBribes(q);
+		}
+
+		scanner.close();
+	}
+}
