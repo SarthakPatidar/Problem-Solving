11e0a5d2ca3ce9985683c581874bb6275f3e81f8
Patidar <sarthakpatidar@gmail.com>
2019-11-14 22:20:22 +0530

    Added In place array element removal

---
+++ leetCode/InPlaceArrayRemoveElement.java
@@ -0,0 +1,81 @@
+package leetCode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InPlaceArrayRemoveElement {
+	public static int[] stringToIntegerArray(String input) {
+		input = input.trim();
+		input = input.substring(1, input.length() - 1);
+		if (input.length() == 0) {
+			return new int[0];
+		}
+
+		String[] parts = input.split(",");
+		int[] output = new int[parts.length];
+		for (int index = 0; index < parts.length; index++) {
+			String part = parts[index].trim();
+			output[index] = Integer.parseInt(part);
+		}
+		return output;
+	}
+
+	public static String integerArrayToString(int[] nums, int length) {
+		if (length == 0) {
+			return "[]";
+		}
+
+		String result = "";
+		for (int index = 0; index < length; index++) {
+			int number = nums[index];
+			result += Integer.toString(number) + ", ";
+		}
+		return "[" + result.substring(0, result.length() - 2) + "]";
+	}
+
+	public static String integerArrayToString(int[] nums) {
+		return integerArrayToString(nums, nums.length);
+	}
+
+	/*
+	 * 
+	 */
+	public static int removeElement(int[] nums, int val) {
+
+		int count = 0;
+		int n = nums.length;
+
+		for (int i = 0; i < n; i++) {
+			if (nums[i] == val) {
+				for (int j = i + 1; j < n; j++) {
+					if (nums[j] != nums[i]) {
+						int temp = nums[j];
+						nums[j] = nums[i];
+						nums[i] = temp;
+						count++;
+						break;
+					}
+				}
+			} else {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		while ((line = in.readLine()) != null) {
+			int[] nums = stringToIntegerArray(line);
+			line = in.readLine();
+			int val = Integer.parseInt(line);
+
+			int ret = removeElement(nums, val);
+			String out = integerArrayToString(nums, ret);
+
+			System.out.print(out);
+		}
+	}
+}
