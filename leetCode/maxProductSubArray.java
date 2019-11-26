b2918dabb52f177b7096c6aefe683c1e5eec0973
Patidar <sarthakpatidar@gmail.com>
2019-11-26 23:12:41 +0530

    added maximum subarray product

--- leetCode/maxProductSubArray.java
+++ leetCode/maxProductSubArray.java
@@ -2,7 +2,7 @@ package leetCode;
 
 import java.util.*;
 
-public class MaxProductSubArray {
+public class maxProductSubArray {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
